71d3721a753eb942b49c855afd40c4b5beaff7a4
Jaqueline Mondini <145024517+Jaquelindaa@users.noreply.github.com>
2026-09-14 20:15:34 -0300

    feat(home): integra componentes da tela inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import { BrButton } from '@govbr-ds/webcomponents-react';
+import FooterGov from "@/components/layout/FooterGov";
+import HeaderGov from "@/components/layout/HeaderGov";
+import VolunteerHeader from "@/components/pages/VolunteerHeader";
+import VolunteerInformation from "@/components/pages/VolunteerInformation";
 
 export default function Page() {
   return (
-    <BrButton emphasis="primary">Entrar</BrButton>
+    <>
+      <HeaderGov />
+
+      <main id="conteudo-principal">
+        <VolunteerHeader />
+        <VolunteerInformation />
+      </main>
+
+      <FooterGov />
+    </>
   );
 }
